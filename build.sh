4a2207303f3a6465ab09d61a527043e741cec3a9
UmairMuddassir <umairmuddassir1@gmail.com>
2026-04-01 16:54:07 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r chaiheadq/requirements.txt
-python chaiheadq/manage.py collectstatic --noinput
-python chaiheadq/manage.py migrate
+cd chaiheadq
+python manage.py collectstatic --noinput
+python manage.py migrate
