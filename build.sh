2aa722cc19f0afda418c64d1fe58dd211dd034e8
UmairMuddassir <umairmuddassir1@gmail.com>
2026-04-02 12:42:14 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 set -o errexit
 cd chaiheadq
 pip install -r requirements.txt
-python manage.py migrate --run-syncdb
+python manage.py migrate auth
+python manage.py migrate contenttypes
+python manage.py migrate admin
+python manage.py migrate sessions
+python manage.py migrate tweet
