88a814e98219100cd1b95badcdd7c4fa0f197bd7
UmairMuddassir <umairmuddassir1@gmail.com>
2026-04-02 12:22:22 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install -r chaiheadq/requirements.txt
 cd chaiheadq
-python manage.py collectstatic --noinput
-python manage.py migrate
+pip install -r requirements.txt
+python manage.py migrate --run-syncdb
+python manage.py collectstatic --noinput --clear
